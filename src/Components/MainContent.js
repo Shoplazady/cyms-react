adc748c355f6b320304971f8fdbd80b1ed6c5e5f
Shoplazady <plouckza.001@gmail.com>
2024-01-19 11:31:58 +0700

    Cyms-react

---
+++ src/Components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+  <>
+    <main>
+      
+    </main> 
+  </>
+  );
+};
+
+export default MainContent;
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+
+    return (
+      <>
+        <aside>
+          
+          </aside>
+      </>
+    );
+  };
+export default Sidebar;
